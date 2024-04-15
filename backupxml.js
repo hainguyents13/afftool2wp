64eb5d9196452d5c437781261b26cdb1a5f1890d
Hải Nguyễn <hainguyen.ts13@gmail.com>
2024-04-15 12:19:29 +0700

    fix attr

--- backupxml.js
+++ backupxml.js
@@ -105,7 +105,8 @@ try {
           // replace images src urls
           $('img').each(img => {
             const src = $(img).attr('src')
-            if (src && src.indexOf('/') == 0) {
+            console.log(src)
+            if (src && src.indexOf('/upload') == 0) {
               console.log(src)
               $(img).attr('src', src.replace('/upload', '/wp-content/uploads'))
               console.log("=> ", $(img).attr('src'))
