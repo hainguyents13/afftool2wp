4c4fb58798fa779c464f3d8ff78741c9df1299c6
Hải Nguyễn <hainguyen.ts13@gmail.com>
2024-04-17 00:16:52 +0700

    add clack

--- backupxml.sh
+++ backupxml.sh
@@ -13,16 +13,17 @@ do
   if [[ ! -d "/web/$f/" ]]; then
     echo "❎ Folder does not exists"
   else
+    echo "hello"
     # Backup content
-    cp backupxml.js /web/$f/
-    cd /web/$f/
-    # yarn add --dev @clack/prompts picocolors adm-zip
-    node backupxml.js $dir $f
-    mv backup.xml $dir/out/$f.xml
-    # Backup upload folder
-    cd /web/$f/upload
-    zip -rq $f.zip .
-    mv $f.zip $dir/out/$f.zip
+    # cp backupxml.js /web/$f/
+    # cd /web/$f/
+    # # yarn add --dev @clack/prompts picocolors adm-zip
+    # node backupxml.js $dir $f
+    # mv backup.xml $dir/out/$f.xml
+    # # Backup upload folder
+    # cd /web/$f/upload
+    # zip -rq $f.zip .
+    # mv $f.zip $dir/out/$f.zip
   fi
 
   echo "[?] Continue? (y/n)"
