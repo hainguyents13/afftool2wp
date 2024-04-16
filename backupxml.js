2a3e780b52a4463445e820bd020023be9fa906fb
Hải Nguyễn <hainguyen.ts13@gmail.com>
2024-04-17 00:24:54 +0700

    fix clack

--- backupxml.js
+++ backupxml.js
@@ -80,8 +80,8 @@ async function askIfContinue() {
 
 async function main() {
   const [root_folder, backup_folder] = process.argv.slice(2);
-  out_folder = path.join(_root_folder, "out")
-  web_folder = path.join(_root_folder, _backup_folder)
+  out_folder = path.join(root_folder, "out")
+  web_folder = path.join(root_folder, backup_folder)
   console.clear()
 
   console.log("out_folder", out_folder)
