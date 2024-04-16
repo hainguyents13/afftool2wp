11aadfb94d054fabfd4b9094a08d252f050bb0e0
Hải Nguyễn <hainguyen.ts13@gmail.com>
2024-04-17 00:18:29 +0700

    add clack

--- backupxml.sh
+++ backupxml.sh
@@ -7,25 +7,24 @@ fi
 next="y"
 while [ "$next" = "y" ]
 do
-  echo "$next"
+  echo ""
   echo "[?] Enter folder name:"
   read f
   if [[ ! -d "/web/$f/" ]]; then
     echo "❎ Folder does not exists"
   else
-    echo "hello"
     # Backup content
-    # cp backupxml.js /web/$f/
-    # cd /web/$f/
-    # # yarn add --dev @clack/prompts picocolors adm-zip
-    # node backupxml.js $dir $f
-    # mv backup.xml $dir/out/$f.xml
+    cp backupxml.js /web/$f/
+    cd /web/$f/
+    yarn add --dev @clack/prompts picocolors adm-zip
+    node backupxml.js $dir $f
+    mv backup.xml $dir/out/$f.xml
     # # Backup upload folder
-    # cd /web/$f/upload
-    # zip -rq $f.zip .
-    # mv $f.zip $dir/out/$f.zip
+    cd /web/$f/upload
+    zip -rq $f.zip .
+    mv $f.zip $dir/out/$f.zip
   fi
-
+  echo ""
   echo "[?] Continue? (y/n)"
   read next
 done
